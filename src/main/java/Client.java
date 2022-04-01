0437b53c7a0321cbf0311b3a06931c6b9db12a23
karl Ruul <karlkevin.ruul@gmail.com>
2022-04-01 19:04:17 +0300

    serveri ja kliendi alge

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Client {
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class Client implements Runnable {
+    private final String address;
+    private final int port;
+
+    public Client(String address, int port) {
+        this.address = address;
+        this.port = port;
+    }
+
+    /**
+     * Start client side thread
+     */
+    @Override
+    public void run() {
+        try {
+            InetSocketAddress hostAddress = new InetSocketAddress(address, port);
+            SocketChannel client = SocketChannel.open(hostAddress);
+
+            System.out.println("Client is now alive");
+
+            String threadName = Thread.currentThread().getName();
+
+            // Send messages to server
+            String [] messages = new String []
+                    {threadName + ": test1",threadName + ": test2",threadName + ": test3"};
+
+            for (String s : messages) {
+                byte[] message = s.getBytes();
+                ByteBuffer buffer = ByteBuffer.wrap(message);
+                client.write(buffer);
+                System.out.println(s);
+                buffer.clear();
+                Thread.sleep(5000);
+            }
+            client.close();
+        } catch (IOException | InterruptedException e) {
+            // TODO: error handling
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    static final String ADDRESS = "localhost";
+    static final int PORT = 1337;
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable server = new Server(ADDRESS, PORT);
+        Runnable testClient = new Client(ADDRESS, PORT);
+
+        new Thread(server).start();
+        Thread.sleep(1000);
+        new Thread(testClient).start();
+    }
 }
