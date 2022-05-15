a203483c971064f3b5ae5f8ad03785c9cbf5035e
AloMartinP <73949637+AloMartinP@users.noreply.github.com>
2022-05-15 15:28:52 +0300

    generator with filename input

--- src/main/java/com/ut/oop/TestTextGenerator.java
+++ src/main/java/com/ut/oop/TestTextGenerator.java
@@ -11,13 +11,13 @@ import java.nio.file.Paths;
 import java.util.Random;
 
 public class TestTextGenerator {
-    public static void generate() {
+    public static void generate(String filename) {
 
         Logger logger = LoggerFactory.getLogger(Server.class);
 
         Path workingDir = Paths.get(System.getProperty("user.dir"));
         while (true) {
-            try (PrintWriter pw = new PrintWriter(new FileWriter(workingDir + "\\src\\dummy.txt", true), true)) {
+            try (PrintWriter pw = new PrintWriter(new FileWriter(workingDir + "\\src\\" + filename, true), true)) {
                 pw.println(new Random().nextDouble());
                 Thread.sleep(1500);
             } catch (IOException | InterruptedException e) {
